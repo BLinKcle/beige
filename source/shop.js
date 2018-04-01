56e586e6783010939f2057f402f47b9890af81bb
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 18:00:21 +0800

    Update shop.js

--- source/shop.js
+++ source/shop.js
@@ -4,7 +4,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Shop")
   .setColor(3447003)
   .setDescription("FAQ's about the Shop")
-  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
+  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/DhkLaDB.png")
   .setTimestamp()
   
