9c1d2410d0d29ee574182f62d883175ede4e9f58
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 17:59:31 +0800

    Update roadshow.js

--- source/roadshow.js
+++ source/roadshow.js
@@ -6,7 +6,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Roadshow")
   .setColor(3447003)
   .setDescription("FAQ's about the Roadshow")
-  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
+  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/qcn745p.png")
   .setTimestamp()
   
