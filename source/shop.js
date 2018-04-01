ced769264093db2f63daa24d59d9b8fe18eaa3dc
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 17:26:25 +0800

    Update shop.js

--- source/shop.js
+++ source/shop.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Roadshow")
+  .setTitle("Shop")
   .setColor(3447003)
-  .setDescription("FAQ's about the Roadshow")
+  .setDescription("FAQ's about the Shop")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
-  .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
+  .setThumbnail("https://i.imgur.com/DhkLaDB.png")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
