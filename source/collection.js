3b3bd269b3a9fccea212d30561e5da19ced87cfb
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 17:51:53 +0800

    Update collection.js

--- source/collection.js
+++ source/collection.js
@@ -5,7 +5,7 @@ var embed = new Discord.RichEmbed()
   .setColor(3447003)
   .setDescription("FAQ's about the Roadshow")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
-  .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
+  .setThumbnail("https://i.imgur.com/sAmOOsM.png")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
