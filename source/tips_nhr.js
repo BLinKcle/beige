09041a11ffbb463c4acc0df04da57a54fc5c0667
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 14:32:11 +0800

    Update tips_nhr.js

--- source/tips_nhr.js
+++ source/tips_nhr.js
@@ -4,7 +4,7 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Tips by users on events")
+  .setTitle("Tips by users on NHR")
   .setColor("97f7ff")
 
 .addField("Don't spend too much on this district, they just give you few NHR Seals (average of 15) in every song. You could just focus on achievements having an all combo in every songs.", "by KrispyKim", true)
