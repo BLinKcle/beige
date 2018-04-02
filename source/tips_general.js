30314443d7ca67ce2c8c20f9ff36f248c6748dfe
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 15:23:15 +0800

    Update tips_general.js

--- source/tips_general.js
+++ source/tips_general.js
@@ -1,1 +1,17 @@
 
+// ladder.js
+//Make sure that there is a ; at the end of last .addField
+
+const Discord = require("discord.js");
+
+var embed = new Discord.RichEmbed()
+  .setTitle("General tips by users when playing BEY")
+  .setColor("97f7ff")
+
+.addField("If you're aiming to have an all sperfect , try these songs: WINNER - empty (Highly recommended); JINUSEAN - gasoline, phone number; TAEYANG - Empty Road, Naked; IKON - Airplane.", "by KrispyKim", true)
+
+
+;
+module.exports = Object.freeze({
+  embed: {embed}
+});
