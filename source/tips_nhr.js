3431c1395cd416a8e9209c598e47967fa98806a4
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 14:30:22 +0800

    Update tips_nhr.js

--- source/tips_nhr.js
+++ source/tips_nhr.js
@@ -7,7 +7,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on events")
   .setColor("97f7ff")
 
-.addField("Don't spend too much on this district, they just give you few NHR Seals (average of 15) in every songs. And you only have a max of 5 tickets daily (total of 75 seals assumption a day). You could just focus on achievements having an all combo in every songs.", "by KrispyKim", true)
+.addField("Don't spend too much on this district, they just give you few NHR Seals (average of 15) in every song. You could just focus on achievements having an all combo in every songs.", "by KrispyKim", true)
 
 
 ;
