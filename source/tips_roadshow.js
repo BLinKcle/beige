21941d682788f765af5a0ba869eaadbdc54f60ba
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 16:29:20 +0800

    Update tips_roadshow.js

--- source/tips_roadshow.js
+++ source/tips_roadshow.js
@@ -6,7 +6,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on roadshow")
   .setColor("97f7ff")
 
-.addField("You can unlock the song list in every district if you have an extra ticket that won't be used on that day; this ensures that you have extra songs that can contribute to seals events when you do get the time (and when they come around)", "by KrispyKim", true)
+.addField("You can unlock the song list in every district if you have an extra ticket that won't be used on that day; this ensures that you have extra songs that can contribute to seals events when you do get the time (and when they come around)", "by freeman", true)
 
 
 ;
