e87293d31bd3c324d4e9ae2b9c08c7d8a5f23caa
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 14:39:19 +0800

    Update and rename tips_roadshow.txt to tips_roadshow.js

---
+++ source/tips_roadshow.js
@@ -0,0 +1,15 @@
+//Make sure that there is a ; at the end of last .addField
+
+const Discord = require("discord.js");
+
+var embed = new Discord.RichEmbed()
+  .setTitle("Tips by users on roadshow")
+  .setColor("97f7ff")
+
+.addField("You can unlock the song list in every district if you have an extra ticket that won't be used on that day; this ensures that you have extra songs that can contribute to seals events when you do get the time (and when they come around)", "by KrispyKim", true)
+
+
+;
+module.exports = Object.freeze({
+  embed: {embed}
+});
