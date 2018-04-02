4cecb3906c15d5a9388515e29e83c765385c9e83
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 12:55:26 +0800

    Create tips_songs.js

---
+++ source/tips_songs.js
@@ -0,0 +1,17 @@
+// ladder.js
+//Make sure that there is a ; at the end of last .addField
+
+const Discord = require("discord.js");
+
+var embed = new Discord.RichEmbed()
+  .setTitle("Tips by users on playing the songs")
+  .setColor("97f7ff")
+
+.addField("Be careful on those sensitive songs (specifically those fast songs). you would notice that the slider counts fast which means it is sensitive. you mostly get perfect rather than sperfect.", "by KrispyKim", true)
+
+.addField("be careful on those sliders in a sensitive songs. you might miss a note at the BEGINNING and END of the sliders.", "by KrispyKim", true)
+
+;
+module.exports = Object.freeze({
+  embed: {embed}
+});
