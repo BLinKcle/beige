c0b95298d352f9f8bc848b59de5a6e23d8679464
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 17:18:23 +0800

    Update event.js

--- source/event.js
+++ source/event.js
@@ -4,7 +4,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Management")
   .setColor("97f7ff")
   .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
-  .setThumbnail("https://i.imgur.com/gvid13v.png")
+  .setThumbnail("https://i.imgur.com/GTtl8Kr.png")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
