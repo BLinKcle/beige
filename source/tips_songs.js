5c0ba2abbad5a7c9c3547e8ed593f28e52bcf918
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 15:20:02 +0800

    Update tips_songs.js

--- source/tips_songs.js
+++ source/tips_songs.js
@@ -11,6 +11,18 @@ var embed = new Discord.RichEmbed()
 
 .addField("Be careful on those sliders in a sensitive songs. You might miss a note at the BEGINNING and END of the sliders.", "by KrispyKim", true)
 .addField("There are times you got all combo but it seems you have missed some notes comparing to other players. Watch those sliders (beginning and end) and stars", "by KrispyKim", true)
+.addField("Star notes don't need to be tapped on the first note. These notes will always give an S-PERFECT. In addition to that, missing star notes won't count as a miss.","by sekigan",true)
+.addField("Although star notes do not count as a miss, missing too many can bring down your rank from an S-->A (for example), so make sure you get them regardless","by freeman",true)
+.addField("Although, it isn't easy to control, but for missing a note or two in the beginning or (near) end of a song can still give S-rank, while missing only one note in the middle of the song usually gives A-rank.","by Locya",true)
+
+
+
+
+.addField("The predicted requirement of getting S rank are not less than 85% combo or not more than 5% great or 2% good. For example, if a song full combo is 1000. You need to get min 850 combo, max. 50 great or 20 good. (If you have good artist cards, the % should be different (not tested)","Ricky_blink",true)
+.addField("Combo checkpoints: 60, 120, 180, 360, 540, 720, 900, 1080, 1260, 1440, 1620, and so on. This can be a few notes off depending on how well you're playing. You may get that first checkpoint at 58 notes if you always get S-PERFECT.","by sekigan",true)
+
+.addField("Watch out for songs such as Epik High - Spoiler and Bigbang - Loser (Live). Just when you thought the song is done, you will still need to wait for a few notes.","by sekigan",true)
+.addField("There is a very thin yellow line at the very top of the screen that moves when playing the songs. You can look at it to know where you are in the song." "by BLinKcle", true)
 
 ;
 module.exports = Object.freeze({
