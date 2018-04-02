19083ee62e8a489810ec1ef70454f9ef25dbb1a6
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 13:29:27 +0800

    Update tips_songs.js

--- source/tips_songs.js
+++ source/tips_songs.js
@@ -7,7 +7,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on playing the songs")
   .setColor("97f7ff")
 
-.addField("Be careful on those sensitive songs (specifically those fast songs). you would notice that the slider counts fast which means it is sensitive. you mostly get perfect rather than sperfect.", "by KrispyKim", true)
+.addField("Be careful on those sensitive songs (specifically those fast songs). You would notice that the slider counts fast which means it is sensitive. You mostly get perfect rather than sperfect.", "by KrispyKim", true)
 
 .addField("Be careful on those sliders in a sensitive songs. You might miss a note at the BEGINNING and END of the sliders.", "by KrispyKim", true)
 
