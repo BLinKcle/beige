0694e3bc00232f8169868a001596dba965ca7491
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 17:00:42 +0800

    Update management.js

--- source/management.js
+++ source/management.js
@@ -4,15 +4,25 @@ var embed = new Discord.RichEmbed()
   .setTitle("Ladder FAQ")
   .setColor("97f7ff")
   .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
+  .setThumbnail("https://i.imgur.com/qCItN2L.png")
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
   
   
-  .addField("Why are Minzy and Bom not in 2ne1 and Taehyun not in Winner?",
-    "Minzy,Bom, and Taehyun are not part of YG anymore so BEYG have no right to use their pictures.", true);
+  .addField("Why are Minzy and Bom cards not in 2ne1 and Taehyun cards not in Winner?",
+    "Minzy,Bom, and Taehyun are not part of YG anymore so BEYG have no right to use their pictures.", true)
 
 
 
+
+
+
+
+
+
+
+
+;
 module.exports = Object.freeze({
   embed: {embed}
 });
