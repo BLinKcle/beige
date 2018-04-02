a2674d1ff8250f7cee291fdf2f85b8bcafe7f2f4
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 16:24:51 +0800

    Update and rename tips_ranking.txt to tips_ranking.js

---
+++ source/tips_ranking.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+var embed = new Discord.RichEmbed()
+  .setTitle("Tips by users on events")
+  .setColor("97f7ff")
+
+.addField("Try playing the games first in training before playing in the ranking challenge. Unequip all your cards so you can compare them with the players in ranking.", "by Locya", true)
+
+
+
+
+
+
+
+
+
+
+
+;
+module.exports = Object.freeze({
+  embed: {embed}
+});
