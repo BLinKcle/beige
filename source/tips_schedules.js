6e6f3b0c48425ef9a15085818d9e7b2844b2e0f7
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-02 13:45:23 +0800

    Update tips_schedules.js

--- source/tips_schedules.js
+++ source/tips_schedules.js
@@ -8,9 +8,10 @@ var embed = new Discord.RichEmbed()
   .setColor("97f7ff")
 
 .addField("If you know you are going to fail your schedule, you can force close the app and retry the schedule.", "by PhoenixKnightZ", true)
+
+.addField("Equip your best cards during recording to get more rewards. You will get 7000 mcoins, 14 exp cards (sometimes you can get gold exp) and more support items.", "by Debby", true)
 .addField("Normal members can get 4nos. of schedules. platinum members can get 1 more.See picture below.", "by Ricky_blink", true)
  .setImage("https://cdn.discordapp.com/attachments/430210328517804032/430230117697060873/20184212571171.png")
-.addField("equip your best cards during recording to get more rewards. for examples you will get 7000 mcoins, 14 exp cards (sometimes you can get gold exp) and more support items.", "by Debby", true)
 ;
 module.exports = Object.freeze({
   embed: {embed}
