26a719076782a16a5b199229b900f1c9c1e3078f
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 21:13:23 +0800

    Update tips_quest.js

--- source/tips_quest.js
+++ source/tips_quest.js
@@ -6,9 +6,9 @@ var embed = new Discord.RichEmbed()
 
 //.addField("Don't do the 'accumulate obtain 3 artists.' daily task. You could stock up 3 contracts for a gallery event rather than 1 contract. It is just a small amount of contracts but it is very helpful stocking up and have a LUCK.", "by KrispyKim", true)
 //.addField("Obtaining 3 artist could count when you finish a song in a roadshow for the first time, daily log in reward, and others (bday support etc.) so better don't rush finishing a district in roadshow and play 3 songs per day instead.","by KrispyKim",true)
-.addField("KrispyKim:","Don't do the 'accumulate obtain 3 artists.' daily task. You could stock up 3 contracts for a gallery event rather than 1 contract. It is just a small amount of contracts but it is very helpful stocking up and have a LUCK.",true)
-.addField("KrispyKim:","Obtaining 3 artist could count when you finish a song in a roadshow for the first time, daily log in reward, and others (bday support etc.) so better don't rush finishing a district in roadshow and play 3 songs per day instead.", true);
-
+.addField("by KrispyKim:","Don't do the 'accumulate obtain 3 artists.' daily task. You could stock up 3 contracts for a gallery event rather than 1 contract. It is just a small amount of contracts but it is very helpful stocking up and have a LUCK.",true)
+.addField("by KrispyKim:","Obtaining 3 artist could count when you finish a song in a roadshow for the first time, daily log in reward, and others (bday support etc.) so better don't rush finishing a district in roadshow and play 3 songs per day instead.", true)
+.addField("by Locya:","If you have no schedules around 5am kst to complete the schedule quest, you can wait pushing 'get' on you last completed schedule before a new day starts and it’ll count as completing a schedule in the quest. ",true)
 
 
 ;
