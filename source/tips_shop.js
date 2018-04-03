04f0709f8d25d4d72a5134a2daec00897e72a362
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 23:03:27 +0800

    Update tips_shop.js

--- source/tips_shop.js
+++ source/tips_shop.js
@@ -7,9 +7,9 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on shop")
   .setColor("97f7ff")
 
-.addField("If you're aiming to have an all sperfect , try these songs: WINNER - empty (Highly recommended); JINUSEAN - gasoline, phone number; TAEYANG - Empty Road, Naked; IKON - Airplane.", "by KrispyKim", true)
+//.addField("", "", true)
 
-
+.addField("by KrispyKim:","If you're aiming to have an all sperfect , try these songs: WINNER - empty (Highly recommended); JINUSEAN - gasoline, phone number; TAEYANG - Empty Road, Naked; IKON - Airplane.",true)
 ;
 module.exports = Object.freeze({
   embed: {embed}
