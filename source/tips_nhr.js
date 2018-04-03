022615a75e4d16adf1fd23c5c8aa37d0778e668c
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 22:53:52 +0800

    Update tips_nhr.js

--- source/tips_nhr.js
+++ source/tips_nhr.js
@@ -7,9 +7,14 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on NHR")
   .setColor("97f7ff")
 
-.addField("Don't spend too much on this district, they just give you few NHR Seals (average of 15) in every song. You could just focus on achievements having an all combo in every songs.", "by KrispyKim", true)
+//.addField("", "", true)
 
-.addField("If you want to finish NHR as soon as possible, play No Visa (Feat. 김기쁨) (VAN.C Remix) five times. It has the shortest duration among all NHR songs so far (1min 36 s). You can also play Holy Moly Boom Boom (Original Mix) (1min 41s).", "by sekigan", true)
+//.addField("", "", true)
+
+.addField("by KrispyKim:","Don't spend too much on this district, they just give you few NHR Seals (average of 15) in every song. You could just focus on achievements having an all combo in every songs.",true)
+.addField("by sekigan:","If you want to finish NHR as soon as possible, play No Visa (Feat. 김기쁨) (VAN.C Remix) five times. It has the shortest duration among all NHR songs so far (1min 36 s). You can also play Holy Moly Boom Boom (Original Mix) (1min 41s).",true)
+
+
 
 ;
 module.exports = Object.freeze({
