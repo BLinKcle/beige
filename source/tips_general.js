f28fcf15535909741d7e28b212c5705ed11e99e5
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 22:43:22 +0800

    Update tips_general.js

--- source/tips_general.js
+++ source/tips_general.js
@@ -8,8 +8,10 @@ var embed = new Discord.RichEmbed()
   .setTitle("General tips by users when playing BEY")
   .setColor("97f7ff")
 
-.addField(" If you're angry or stressed about the game (having some bugs or other issues), go to #bey-rant . Let your emotions come out about it", "by KrispyKim", true)
-.addField(" When using a bluetooth headset, the wireless connection gives a delay on the music. Go to Settings then adjust the note speed to somewhere between +120 to +130ms.", "by sekigan", true)
+
+.addField("by KrispyKim:","If you're angry or stressed about the game (having some bugs or other issues), go to #bey-rant . Let your emotions come out about it",true)
+.addField("by sekigan:","When using a bluetooth headset, the wireless connection gives a delay on the music. Go to Settings then adjust the note speed to somewhere between +120 to +130ms.",true)
+
 
 ;
 module.exports = Object.freeze({
