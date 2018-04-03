ab6bb7ae2a8f62d4de97b0ec005fdbcf63fecc3e
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 22:56:25 +0800

    Update tips_ranking.js

--- source/tips_ranking.js
+++ source/tips_ranking.js
@@ -4,8 +4,9 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on events")
   .setColor("97f7ff")
 
-.addField("Try playing the games first in training before playing in the ranking challenge. Unequip all your cards so you can compare them with the players in ranking.", "by Locya", true)
+//.addField(".", "", true)
 
+.addField("by Locya:","Try playing the games first in training before playing in the ranking challenge. Unequip all your cards so you can compare them with the players in ranking.",true)
 
 
 
