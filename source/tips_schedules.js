b1c775b87d851312c058aa1fccc054820ba5c501
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 21:21:43 +0800

    Update tips_schedules.js

--- source/tips_schedules.js
+++ source/tips_schedules.js
@@ -13,6 +13,16 @@ var embed = new Discord.RichEmbed()
 .addField("Normal members can get 4nos. of schedules. platinum members can get 1 more.See picture below.", "by Ricky_blink", true)
  .setImage("https://cdn.discordapp.com/attachments/430210328517804032/430230117697060873/20184212571171.png")
 ;
+var data = new Discord.RichEmbed()
+ .setColor("97f7ff")
+.addField("by Ricky_blink", " Rewards of different types of schedule.(should be independent of card influence)_see picture below.", true)
+ .setImage("https://media.discordapp.net/attachments/430210328517804032/430662328212783115/2018-04-03_17h27_44.png")
+
+;
 module.exports = Object.freeze({
   embed: {embed}
-});
+})
+module.exports = Object.freeze({
+  embed: {data}
+})
+;
