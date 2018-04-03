81227285c2bfb669a0b4052fc0eaaca9728edc53
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 22:48:49 +0800

    Update tips_ladder.js

--- source/tips_ladder.js
+++ source/tips_ladder.js
@@ -7,9 +7,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on ladder")
   .setColor("97f7ff")
 
-//.addField("", "", true)
 
-//.addField(" ", "", true)
 
 .addField("by freeman:","If you feel like you're about to lose to your opponent, you can force quit your app. You will lose one star as a result, but you will keep your streak of +2 stars per win. (if you had a streak going on, that is)",true)
 .addField("by KrispyKim:","You could forcely quit the app even searching the opponent and see who and what the song is. I know that you know who have higher cards and you want a particular song because you have good cards on that artist. Nothing to loose nor gain stars",true)
