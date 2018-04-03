35b4690eefbb113a4603c0ec09cb431ee7ba70ca
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-03 22:37:53 +0800

    Update tips_event.js

--- source/tips_event.js
+++ source/tips_event.js
@@ -7,9 +7,10 @@ var embed = new Discord.RichEmbed()
   .setTitle("Tips by users on events")
   .setColor("97f7ff")
 
-.addField("If for whatever reason you feel that you might not have enough days to clock attendance for 9-grid events, you can wait until the patch implementing it is over before logging in, or when someone here in discord mentions it being up.", "by freeman", true)
-.addField(" Purchasing memberships do count toward 'Buy Diamonds 1 times' for 9-grid events", "by freeman", true)
-
+//.addField("", "by freeman", true)
+//.addField(" ", "by freeman", true)
+.addField("by freeman:","If for whatever reason you feel that you might not have enough days to clock attendance for 9-grid events, you can wait until the patch implementing it is over before logging in, or when someone here in discord mentions it being up.",true)
+.addField("by freeman","Purchasing memberships do count toward 'Buy Diamonds 1 times' for 9-grid events.",true)
 ;
 module.exports = Object.freeze({
   embed: {embed}
