2c8f3cadbbf2f7ad227c863293ecd9f48ca905e2
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 21:00:55 +0800

    Update training.js

--- source/training.js
+++ source/training.js
@@ -10,13 +10,13 @@ var embed = new Discord.RichEmbed()
   
   
   .addField("Q: What are the shortest songs?",
-    "A: Some of the shortest songs are Missing You(GD), Lies Remix Ver (BIGBANG), Bad (Epik High), YG Family Bounce(YG Family)", true);
-/*
-    .addField("Q: Can we pick the song(s) we want to play?", "A: No, the songs are pre-determined and one random song will be chosen for you from the song list.", true);
-  /*
-   * Blank field, useful to create some space..addBlankField(true)
-   */
+    "A: Some of the shortest songs are Missing You(GD), Lies Remix Ver (BIGBANG), Bad (Epik High), YG Family Bounce(YG Family)", true)
+.addField("Q: What will I get when I play on training?","A: The reward for playing in training is M-coins. The better you play a song and the longer the song, the better the reward.",true)
 
+
+
+
+;
 
 module.exports = Object.freeze({
   embed: {embed}
