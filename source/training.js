b086d4554d88b66b3c66415f92341ecaf507a9c2
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 20:55:42 +0800

    Update training.js

--- source/training.js
+++ source/training.js
@@ -1,18 +1,16 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Roadshow")
+  .setTitle("Training")
   .setColor(3447003)
-  .setDescription("FAQ's about the Roadshow")
-  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/20dsUK3.png")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
   
   
-  .addField("Q: How often are the songs updated?",
-    "A: Once every 48 hours.  It is worth mentioning that the songs will contain Platinum-only songs, so non-Platinum members get to experience them too.", true);
+  .addField("Q: What are the shortest songs?",
+    "A: Some of the shortest songs are Missing You(GD), Lies Remix Ver (BIGBANG), Bad (Epik High), YG Family Bounce(YG Family)", true);
 /*
     .addField("Q: Can we pick the song(s) we want to play?", "A: No, the songs are pre-determined and one random song will be chosen for you from the song list.", true);
   /*
