e50debeeee4f88e8278296b55dbe988e33357e96
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 22:14:46 +0800

    Update club.js

--- source/club.js
+++ source/club.js
@@ -1,9 +1,9 @@
 
 const Discord = require("discord.js");
 
-var embed = new Discord.RichEmbed()
-   .setTitle("Club FAQ")
+  .setTitle("Club FAQ")
   .setColor("97f7ff")
+  .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/60nlVAF.png")
   .setTimestamp()
   
