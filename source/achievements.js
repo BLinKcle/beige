7de57424459999d9dbb69edcc1d55423981af4a1
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 21:26:10 +0800

    Update achievements.js

--- source/achievements.js
+++ source/achievements.js
@@ -2,10 +2,8 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Achievements")
-  .setColor(3447003)
-  .setDescription("FAQ's about Achievements")
-  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
+  .setTitle("Achievements FAQ")
+  .setColor("97f7ff")
   .setThumbnail("https://i.imgur.com/gvid13v.png")
   .setTimestamp()
   
