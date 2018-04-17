7b5d3b0d2e9befde8ca2c5ba4977c9db3e673213
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 22:13:23 +0800

    Update achievements.js

--- source/achievements.js
+++ source/achievements.js
@@ -2,9 +2,10 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Achievements FAQ")
+  .setTitle("Achievement FAQ")
   .setColor("97f7ff")
-  .setThumbnail("https://i.imgur.com/gvid13v.png")
+  .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
+  .setThumbnail("https://imgur.com/M8m2GN2")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
