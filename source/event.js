7a10ab774e1a3dcd92d7009ead84eef1133d92c1
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 10:43:23 +0800

    Update event.js

--- source/event.js
+++ source/event.js
@@ -10,8 +10,9 @@ var embed = new Discord.RichEmbed()
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
   
   
-  .addField("In special rewards event, will I get 10 artist cards for the 680 diamond advanced contract folder?",
-    "Yes. Advanced Contract folder in the event is the same as using 10 Advanced contract folder in shop. In other words, you can choose still choose which artist you prefer.", true)
+  .addField("Q: In special rewards event, will I get 10 artist cards for the 680 diamond advanced contract folder?",
+    "A: Yes. Advanced Contract folder in the event is the same as using 10 Advanced contract folder in shop. In other words, you can choose still choose which artist you prefer.", true)
+.addField("Q: In Limited Edition(LE) event, is it possible to not get the possible pictures shown? ","The probability of getting the actual picture card is very low. It is possible to not get the pictures of reward shown or to not get an artist card at all. ",true)
 
 
   
