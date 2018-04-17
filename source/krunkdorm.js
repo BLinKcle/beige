e4177128378945b354ea6c4dd5702b57a412e310
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 22:25:39 +0800

    Update krunkdorm.js

--- source/krunkdorm.js
+++ source/krunkdorm.js
@@ -4,7 +4,7 @@ var embed = new Discord.RichEmbed()
   .setTitle("Krunk Dorm FAQ")
   .setColor("97f7ff")
   .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
-  .setThumbnail("https://i.imgur.com/gvid13v.png")
+  .setThumbnail("https://i.imgur.com/9PgTV6I.png")
   .setTimestamp()
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
