2e74fdfaad3530b8a7d56ed142f17a9bc8b05990
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 22:28:34 +0800

    Update ranking.js

--- source/ranking.js
+++ source/ranking.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Ranking")
-  .setColor(3447003)
-
+    .setTitle("Achievement FAQ")
+  .setColor("97f7ff")
+  .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/WeedNeG.png")
   .setTimestamp()
   
