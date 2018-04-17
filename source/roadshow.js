830fc8aba4992fb368eb3a0e8e724eef90592c60
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-17 21:12:25 +0800

    Update roadshow.js

--- source/roadshow.js
+++ source/roadshow.js
@@ -5,7 +5,6 @@ const Discord = require("discord.js");
 var embed = new Discord.RichEmbed()
   .setTitle("Roadshow")
   .setColor(3447003)
-  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/gvid13v.png")
   .setThumbnail("https://i.imgur.com/qcn745p.png")
   .setTimestamp()
   
@@ -16,7 +15,7 @@ var embed = new Discord.RichEmbed()
     "A: Every district have a required number of stars to unlock. Collect the stars by completing the missions assigned at each song.", true)
   .addField("Q: What is the easiest song on expert?","A:Some easy expert songs are:MTBD(2ne1), Airplane(iKon), Empty(WINNER),Zutter(BIGBANG), A-Yo(Jinusean), Falling in Love (2ne1), Ride(Taeyang), Empty Road(Taeyang), Fire(2ne1)",true)
 .addField("Q: How do you obtain stage cards?","A: Three stage cards are automatically given every refresh at 5am (China).",true)
-
+.addField("Q: Why are some of my friends have Hanyang Station?","Hanyang station is the last station in the pre-global release.",true)
 ;
 
 module.exports = Object.freeze({
