14ac62f21f22f9d60dcfb998cf02a6a356c9072b
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-19 15:56:23 +0800

    Update event.js

--- source/event.js
+++ source/event.js
@@ -12,7 +12,7 @@ var embed = new Discord.RichEmbed()
   
   .addField("Q: In special rewards event, will I get 10 artist cards for the 680 diamond advanced contract folder?",
     "A: Yes. Advanced Contract folder in the event is the same as using 10 Advanced contract folder in shop. In other words, you can choose still choose which artist you prefer.", true)
-.addField("Q: In Limited Edition(LE) event, is it possible to not get the possible pictures shown? ","A: The probability of getting the actual picture card is very low. It is possible to not get the pictures of reward shown or to not get an artist card at all. ",true)
+.addField("Q: In Limited Edition(LE) event, is it possible to not get the pictures shown? ","A: The probability of getting the actual picture card is very low. It is possible to not get the pictures of reward shown or to not get an artist card at all. ",true)
 .addField("Q: In seals event, what is the use of the non-artist card? ","A: It's an exp card equivalent to 1 star gold exp card, 1500exp.  ",true)
 .addField("Q: What is the best way to pull for LE event?","A: There is no best way as single pulls and 10 pulls have the same reward probability of 10%. However, there is a higher probability to get an artist card for 10 pulls rather than single pull.",true)
 .addField("Q: In Collect Artist Gallery event, I already have the cards required, do I need to get it again?","A: Yes, pictures of cards you got before the event are not counted. You will need to collect them again.",true)
