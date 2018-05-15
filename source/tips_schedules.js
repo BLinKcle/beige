437b03c79c21390bb2aec175c39db9f3c9c1a2ee
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-15 15:41:47 +0800

    Update tips_schedules.js

--- source/tips_schedules.js
+++ source/tips_schedules.js
@@ -13,6 +13,8 @@ var embed = new Discord.RichEmbed()
 //.addField("", "", true)
 .addField("by PhoenixKnightZ:","If you know you are going to fail your schedule, you can force close the app and retry the schedule.",true)
 .addField("by Debby:","Equip your best cards during recording to get more rewards. You will get 7000 mcoins, 14 exp cards (sometimes you can get gold exp) and more support items.",true)
+.addField("by BLinKcle:","To get the maximum rewards for tv schedules, you need at least 4900 total influence.",true)
+.addField("by BlinKcle:", "If you get 14 exp, that means you got 1 gold card, 2 2-star exp, and 11 1-star exp cards. If you get 13 exp cards, it means you got 11 1-star exp cards, and 2 2-star exp cards.",true)
 .addField("by Ricky_blink:","Normal members can get 4nos. of schedules. platinum members can get 1 more.See picture below.",true)
 
 
