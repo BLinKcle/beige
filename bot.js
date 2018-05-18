018abf948ccf247b40d37543c1bba8842f1d5f12
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-18 21:58:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,7 +117,8 @@ client.on('message', async message => {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
-	     client.channels.get(429470999692705794).send("Category:  "+ (cat));
+	     const mychan = 429474095227469855;
+	     client.channels.get(mychan).send("Category:  "+ (cat));
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
