746df1deda6673e5140fa5181bbb3ae8703d88eb
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-18 21:50:56 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,11 +113,11 @@ client.on('message', async message => {
 //make beige talk
      if(command === "say") {     
 	     //for mods and admin only
-	if(!message.member.roles.some(r=>["Admin", "Mods"].includes(r.name)))
-		return message.reply("Sorry, you don't have permissions to use this!");
+	
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
+	     client.channels.get(429470999692705792).send("Category:  "+ (cat));
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
