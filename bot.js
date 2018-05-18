ab5a43609f920941bcbb2877712e748d5175a569
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-18 22:10:58 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,8 +122,9 @@ client.on('message', async message => {
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
+//    message.channel.send(sayMessage);
 // for reading text         
+	     client.channels.find("name","general").send("Welcome!")
          }
 	
 	
