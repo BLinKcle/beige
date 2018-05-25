d8125450d9a0b28bebfbe09e7df4c01c7fb0462c
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-26 07:07:21 +0800

    Update schedule.js

--- source/schedule.js
+++ source/schedule.js
@@ -12,8 +12,8 @@ var embed = new Discord.RichEmbed()
   
   .addField("Q: When I tap on collect reward, it says 'Schedule expired' Any idea what this means?",
     "A: This is a glitch in the game. Try restarting or check if the reward is already added in your items.", true)
-.addField("How to get gold exp card from TV schedule?","You need an influence of at least 4.9k to at least get 2 2star exp cards or a gold exp. Getting a gold exp is random.",true)
-.addField("Are there songs available on schedule only?","Yes, some songs can only be played on schedule. Some examples are Artificial Grass(AKMU), Come Back Home(2ne1), You and I(2ne1), and Wings(Bigbang).",true)
+.addField("Q: How to get gold exp card from TV schedule?","A: You need an influence of at least 4.9k to at least get 2 2star exp cards or a gold exp. Getting a gold exp is random.",true)
+.addField("Q: Are there songs available on schedule only?","A: Yes, some songs can only be played on schedule. Some examples are Artificial Grass(AKMU), Come Back Home(2ne1), You and I(2ne1), and Wings(Bigbang).",true)
 ;
 module.exports = Object.freeze({
   embed: {embed}
