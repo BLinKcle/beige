0a5dc0faf60da3e115092e84a0d0fad02fbde561
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-26 07:06:18 +0800

    Update management.js

--- source/management.js
+++ source/management.js
@@ -14,8 +14,8 @@ var embed = new Discord.RichEmbed()
   .addField("Q: Which card is the best to level up?","A: Unless you have a card that you want to really use, the best card to upgade is rank 1 trainee card if you want to save on m-coins and material. However, if you want to save on rank 5 cards, it is best to level up an artist card instead of trainee.",true)
 .addField("Q: Is it worth to promote Artist to Star?", "A: Yes. It gives a higher score in songs due to increase in influence and is required to get the gold badge in achievements.",true)
 .addField("Q: If I promote to trainee to artist, will the level reset?","A: No, the level stays the same. It will be rank 5 level 50 artist. You need to evolve it to get it to rank 6.", true)
-.addField("Q: How do I promote a traine to Artist?","A: To promote a card, you need 2 cards that have the same rank as your card which means you only need a level 1 rank 5 trainee card.",true)
-.addField("Q: Will the influence differ depending on the card I use to upgrade?","A: Yes it's true but only until you evolve the card. Once you evolve the card, it will set to the default influence for that rank and level.",true)
+.addField("Q: How do I promote a trainee to Artist?","A: To promote a card, you need 2 cards that have the same rank as your card which means you only need a level 1 rank 5 trainee card.",true)
+.addField("Q: Will the influence differ depending on the card I use to upgrade?","A: It's true but only until you evolve the card. Once the card evolve, it will set to the default influence for that rank and level.",true)
 .addField("Q: How do I bind to Guess account?","A: Login as Guest> Go to Settings>Go to User Center>Sign up (Will be your new X.D account)>Submit>All Done",true)
 
 
