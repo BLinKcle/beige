55b8d57b4f853a207b7417ebb1e65931eaf9194f
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-05-26 06:22:32 +0800

    Update quest.js

--- source/quest.js
+++ source/quest.js
@@ -4,7 +4,7 @@ var embed = new Discord.RichEmbed()
    .setTitle("Quest FAQ")
   .setColor("97f7ff")
   .setFooter("BeatEvo YG FAQ's","https://i.imgur.com/gvid13v.png")
-  .setThumbnail("https://i.imgur.com/STwPPQS.png")
+  .setThumbnail("https://i.imgur.com/M96lhVN.jpg")
 
   
   //Add your questions here: Make sure there is a ; only at the end of the last .addField
