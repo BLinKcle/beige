1800765b3246cdedc6118a3906c5a8afe0cee6d1
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-07-11 06:27:53 +0800

    Update club.js

--- source/club.js
+++ source/club.js
@@ -12,11 +12,12 @@ var embed = new Discord.RichEmbed()
   
   
   .addField("Q: Is there a card for the top 3 club during group birthdays/anniversary?",
-    "A: There is no card for group birthdays.", true)
+    "A: As of the moment, there is no card for group birthdays.", true)
 .addField("Q: Does the image used in the reward for the top 3 supporting clubs, the gold cards, all the same?","A: Yes. The only difference is the rank of the card. Top 1,2,3 clubs get rank 3,2,1 artist card, respectively.",true)
 .addField("Q: Will I lose all my point when I leave a club or get kicked?","The contributions you get to spend in club shop do not disappear (that stays with you no matter what). However, when you join a new club, your contribution display starts back at 0.",true)
 .addField("Q: Club Upgrading requirements?","A: See the following url: https://www.reddit.com/r/BeatEVOYG/comments/7joepi/club_upgrading_requirement/", true)
 .addField("Q: Do you get rewards after being kicked?", "A: No, only the members that are still a member of the club will get the rewards.", true)
+.addField("Q: After the 3000 M-coin donation, what are the succeeding diamond donations and contribution points equivalent?","The next diamond donation is 25 dias for 125 contribution points. The succeeding donation points will increase by 25 dias and 125 contribution points until the 9 times donation limit is reached.",true)
 .addField("Q: How much can you get daily when donating upto the 9 times donation limit?", "A: You will get 2,775 contribution points. But if the birthday card is already open, you can donate as much as you want for 100 dias with 500 contribution points.")
 
           
